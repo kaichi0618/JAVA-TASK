13ed01a351d3ebbde3dd62963ef17a4d11bd329c
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-31 22:02:51 +0900

    [add]task01

--- src/javaTask/Task01.java
+++ src/javaTask/Task01.java
@@ -2,6 +2,10 @@ package javaTask;
 
 public class Task01 {
 	public static void main(String[] args) {
-		
+		int sum = 0;
+		for (int number = 1; number <= 100; number++) {
+			sum += number;
+			System.out.println("合計は" + sum + "です。");
+		}
 	}
 }
