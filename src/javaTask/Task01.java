60a350a02af07200573aca99cca0c29fbd7575e3
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-31 22:40:17 +0900

    [add]task02

--- src/javaTask/Task01.java
+++ src/javaTask/Task01.java
@@ -2,11 +2,11 @@ package javaTask;
 
 public class Task01 {
 	public static void main(String[] args) {
-//		int sum = 0;
-//		for (int number = 1; number <= 100; number++) {
-//			sum += number;
-//			System.out.println("合計は" + sum + "です。");
-//		}
+		int sum = 0;
+		for (int number = 1; number <= 100; number++) {
+			sum += number;
+		}
+		System.out.println("合計は" + sum + "です。");
 		
 		int sum1 = 0;
 		int num1 = 1;
---
+++ src/javaTask/Task02.java
@@ -0,0 +1,15 @@
+package javaTask;
+
+public class Task02 {
+	public static void main(String[] args) {
+		int num = 1;
+		int sum = 0;
+		while (num <= 100) {
+			++num;
+			if (num%2 == 0) {
+				sum = sum + num;
+			}
+			System.out.println("偶数の和は" + sum);
+		}
+	}
+}
