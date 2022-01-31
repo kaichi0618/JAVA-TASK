9fefb16cb703edfb140a25f1fb96edb5330bc569
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-01-31 22:10:14 +0900

    [add]while

--- src/javaTask/Task01.java
+++ src/javaTask/Task01.java
@@ -2,10 +2,18 @@ package javaTask;
 
 public class Task01 {
 	public static void main(String[] args) {
-		int sum = 0;
-		for (int number = 1; number <= 100; number++) {
-			sum += number;
-			System.out.println("合計は" + sum + "です。");
+//		int sum = 0;
+//		for (int number = 1; number <= 100; number++) {
+//			sum += number;
+//			System.out.println("合計は" + sum + "です。");
+//		}
+		
+		int sum1 = 0;
+		int num1 = 1;
+		while (num1 <= 100) {
+			sum1 = sum1 + num1;
+			num1 = num1 + 1;
+			System.out.println("合計: " + sum1);
 		}
 	}
 }
