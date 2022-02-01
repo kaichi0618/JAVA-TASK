2ebe388a1f6f3a0052e89366796a5e2652ab503b
kaichi0618 <kaichi.1998.0618@gmail.com>
2022-02-01 23:53:36 +0900

    [add]fizzbuzz文

--- src/javaTask/FuzzBuzz.java
+++ src/javaTask/FuzzBuzz.java
@@ -1,5 +1,14 @@
 package javaTask;
 
 public class FuzzBuzz {
-
+	public static void main(String[] args) {
+		int num = 0;
+		int fizz = 3;
+		int buzz = 5;
+		for (num = 1; num <= 100; num++) {
+			if (num % (fizz * buzz) == 0) {
+				System.out.println("fizzbuzz");
+			} else if ()
+		}
+	}
 }
